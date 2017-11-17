b05316097d3e1c09c246d855a13e8db76d2c406c
Adam Fudge <coolfudge@gmail.com>
2017-11-17 22:41:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,12 +17,3 @@ puts req.path
     resp.finish
   end
 end
-
-
-if req.path.match(/items/)
-
-  song_title = req.path.split("/items/").last #turn /songs/Sorry into Sorry
-  song = @@songs.find{|s| s.title == song_title}
-
-  resp.write song.artist
-end
